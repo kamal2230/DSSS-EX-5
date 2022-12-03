ef79a9f9c74d5c8e40b4931e7124b4482113d76e
Kamal <kamalkhumar22@gmail.com>
2022-12-03 20:37:54 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='github_demo',
+    version='0.0.1',
+    packages=['snowflake'],
+    url='',
+    license='Apache-2.0',
+    author='kamal khumar',
+    author_email='',
+    description='demo project performing version control'
+)
